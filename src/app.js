7ed2a554ca5a6a3a4b4798ce81245a4c67a730f0
Thashwin23 <thashwin1234@gmail.com>
2026-09-06 20:47:11 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express(); // Create an instance of the Express application
 
-app.get(
-  "/example",
-  (req, res, next) => {
-    console.log("First handler");
-    // Passes control to the next middleware function or route handler
-    res.send("First handler");
-    next();
-  },
-  (req, res) => {
-    res.send("Second handler");
-  },
-);
+app.use("/admin", (req, res, next) => {
+  console.log("Admin route accessed");
+  const token = "xyz444";
+  const isAuthenticated = token === "xyz"; // Simulated authentication check
+  if (!isAuthenticated) {
+    return res.status(401).send("Unauthorized");
+  }
+  next();
+});
+
+app.get("/admin/getAllUsers", (req, res) => {
+  console.log("Received request to get all users");
+  res.send("This is the response for getAllUsers");
+});
+
+app.delete("/admin/deleteUser", (req, res) => {
+  console.log("Received request to delete a user");
+  res.send("This is the response for deleteUser");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
