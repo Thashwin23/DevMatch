9b2abc4caba817aea59437b5b22dadf2150e0130
Thashwin23 <thashwin1234@gmail.com>
2026-09-06 18:18:56 +0530

    created multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express(); // Create an instance of the Express application
 
-app.use("/hello", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello, from the server!");
-});
+app.get(
+  "/example",
+  (req, res, next) => {
+    console.log("First handler");
+    // Passes control to the next middleware function or route handler
+    res.send("First handler");
+    next();
+  },
+  (req, res) => {
+    res.send("Second handler");
+  },
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
