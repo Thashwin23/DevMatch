89601efcd29af060e3eec6e27c59a222f1599a16
Thashwin23 <thashwin1234@gmail.com>
2026-09-09 00:00:13 +0530

    implemented delete and update apis

--- src/app.js
+++ src/app.js
@@ -13,9 +13,63 @@ app.post("/signup", async (req, res) => {
   } catch (error) {
     res.status(400).send("Error creating user: " + error.message);
   }
+});
 
-  // user.save();
-  // res.send("User created successfully");
+app.delete("/delete", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  try {
+    const result = await User.findByIdAndDelete({ userEmailId });
+    if (!result) {
+      res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("Error deleting user: " + error.message);
+  }
+});
+app.patch("/update", async (req, res) => {
+  const userId = req.body.userId;
+  const updateData = req.body;
+  try {
+    const result = await User.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+    if (!result) {
+      res.status(404).send("User not found");
+    }
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Error updating user: " + error.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmailId });
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error fetching users: " + error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  try {
+    const users = await User.findOne({ emailId: userEmailId });
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error fetching users: " + error.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error fetching users: " + error.message);
+  }
 });
 
 connectDB()
